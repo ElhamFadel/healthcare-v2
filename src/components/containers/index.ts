07a19defe4b923dfaad9b575e8123a0284d0db56
ElhamFadel <elham2000fadel@gmail.com>
2025-01-05 22:37:16 +0200

    UI/ added bottom navbare

--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -3,3 +3,4 @@ export { default as FindDoctorSection } from "./FindDoctorSection";
 export {default as TopDoctors} from "./TopDoctors";
 export {default as TestimoniesSection} from "./TestimoniesSection"
 export {default as AboutUsSection} from "./AboutUsSection"
+export {default as BottomNavBar} from "./BottomNavBar"
--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -1,11 +1,16 @@
 import { UpcomingSchedule, UserHeader,FindYourDoctor } from "@/components/containers/user";
+import {BottomNavBar} from "@/components/containers";
 const UserProfile = () => {
   return (
+    <>
     <div className="container mx-auto p-4">
       <UserHeader />
       <UpcomingSchedule />
       <FindYourDoctor selectedDoctorId={1} />
+      {/* here visualization and progress */}
     </div>
+    <BottomNavBar />
+    </>
   );
 };
 
